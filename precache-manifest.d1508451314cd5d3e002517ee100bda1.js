ede71c1ef8e8dc1d6b767d0f444d0c87f89576f0
Michael Stevens <okyfunoky@hotmail.com>
2019-10-25 19:28:11 -0700

    Updates

---
+++ precache-manifest.d1508451314cd5d3e002517ee100bda1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46fc05e234deffbd38278bc9e9d2e57a",
+    "url": "/clickgame/index.html"
+  },
+  {
+    "revision": "24f609c9c2a144262a9f",
+    "url": "/clickgame/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "76df4d833b87e1ea3574",
+    "url": "/clickgame/static/js/2.d83b082e.chunk.js"
+  },
+  {
+    "revision": "24f609c9c2a144262a9f",
+    "url": "/clickgame/static/js/main.16b750c6.chunk.js"
+  },
+  {
+    "revision": "d8604a1c59ce1558526d",
+    "url": "/clickgame/static/js/runtime-main.874c4d57.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/clickgame/static/media/logo.25bf045c.svg"
+  }
+]);
